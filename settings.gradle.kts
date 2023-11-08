a42ca3adde3d77b2e28f5e2088e192265ca7e013
Lisbeth Ortega <lisbeth_ortega@ucne.edu.do>
2023-11-08 18:08:38 -0400

    firt commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lisbeth_p2_ap2"
+include(":app")
+ 
